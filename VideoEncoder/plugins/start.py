9aba40197c70f8f0895fcbea96056a988aeec3a5
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-11-24 13:23:21 +0530

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -23,9 +23,9 @@ from ..utils.buttons import start, check_user
 
 @Client.on_message(filters.command(['start]))
 async def start_message(app, message):
-    await message.reply(text="OkDa Started", reply_markup=start)
+    await message.reply(text="OkDa Started")
  
 @Client.on_message(filters.command(['help']))
 async def hlp_message(app, message):
-    await message.reply(text="OkDa Helped", reply_markup=start)
+    await message.reply(text="OkDa Helped")
  
